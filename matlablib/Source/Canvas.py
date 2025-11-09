c5450f630dfd049e2c10dbf007e586a9bdb9e61f
kaiewski <babaduk467@gmail.com>
2025-11-09 12:50:15 +0100

    Add matlablib folder.

---
+++ matlablib/Source/Canvas.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from PySide6.QtWidgets import QFrame
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+
+class Canvas(FigureCanvas):
+	"""The canvas basic class."""
+	def __init__(self, parent: QFrame) -> None:
+		"""Canvas class initialization."""
+		self.parent_app = parent
+		self.dpi = 100
+		self.figure = Figure(figsize=(parent.width()/self.dpi, parent.height()/self.dpi), dpi=self.dpi)
+		self.figure.subplots_adjust(left=0, right=1, bottom=0, top=1, wspace=0, hspace=0)
+		
+		self.canvas = FigureCanvas(self.figure)
+		self.axes = self.figure.add_subplot(111, projection='3d')
+		self.clear_plot()
+
+		super().__init__(self.figure)
+
+	def draw_capacitor(self, point_quanity: int, XYZ_start: list, XYZ_end: list, electrode_distance: float) -> None:
+		"""
+		Draw a model of capactior by position and distance between electodes.
+
+		Attributes:
+			point_quanity(int): usualy 4 point to calculate a capacitor model.
+			XYZ_start(list): list of start points of capacitor.
+			XYZ_end(list): list of end points of capactior.
+			electrode_distance(float): distance between electrodes.
+		"""
+
+		[x, y, z] = self.calculate_capacitor_sizes(point_quanity, XYZ_start, XYZ_end, electrode_distance)
+
+		N = point_quanity
+
+		self.axes.fill_between(x[0], y[0], z[0], x[1], y[1], z[1], alpha=0.5, edgecolor='k')
+		self.axes.fill_between(x[2], y[2], z[2], x[3], y[3], z[3], alpha=0.5, edgecolor='k')
+		self.setParent(self.parent_app)
+
+	def calculate_capacitor_sizes(self, point_quanity: int, XYZ_start: list, XYZ_end: list, electrode_distance: float) -> list[list, list, list]:
+		"""Calculate start and end points of capacitor."""
+		x_axis = [XYZ_start[0], XYZ_end[0], XYZ_start[0], XYZ_end[0]]
+		y_axis = [XYZ_start[1], XYZ_end[1], XYZ_start[1], XYZ_end[1]]
+		z_axis = [XYZ_start[2], XYZ_end[2], XYZ_start[2] + electrode_distance, XYZ_end[2] + electrode_distance]
+
+		return[x_axis, y_axis, z_axis]
+
+	def draw_arrow_axes(self, arrow_length: float=0.1, enable_axes: bool=True) -> None:
+		"""Draw an axis arrows if they enabled."""
+		if enable_axes:
+			origin = [0, 0, 0]
+			
+			x_axis = [arrow_length, 0, 0]
+			y_axis = [0, arrow_length, 0] 
+			z_axis = [0, 0, arrow_length]
+
+			colors = ['red', 'green', 'blue']
+			
+			self.axes.quiver(*origin, *x_axis, color=colors[0], arrow_length_ratio=0.1, linewidth=1)
+			self.axes.quiver(*origin, *y_axis, color=colors[1], arrow_length_ratio=0.1, linewidth=1)
+			self.axes.quiver(*origin, *z_axis, color=colors[2], arrow_length_ratio=0.1, linewidth=1)
+			
+			self.axes.text(arrow_length * 1.1, 0, 0, "X", color=colors[0], fontsize=5)
+			self.axes.text(0, arrow_length * 1.1, 0, "Y", color=colors[1], fontsize=5)
+			self.axes.text(0, 0, arrow_length * 1.1, "Z", color=colors[2], fontsize=5)
+
+	def clear_plot(self) -> None:
+		"""Clear an axes and plot."""
+		self.axes.cla()
+		self.axes.set_axis_off()
+		self.axes.set(xlim=(0, 1), ylim=(0, 1), zlim=(0, 1))
+		self.axes.set_aspect('equal', 'box')
---
+++ matlablib/Source/PlotWindow.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QWidget, QSizePolicy
+from Source._windows.plot_window import Ui_Form
+from Source.Canvas import Canvas as Canvas
+
+class PlotWindow(QWidget):
+	"""Main window of the program."""
+
+	def __init__(self) -> None:
+		"""Class initialization."""
+		super(PlotWindow, self).__init__()
+		self.ui = Ui_Form()
+		self.ui.setupUi(self)
+
+		self.initialize_ui()
+		self.draw_basic_model()
+
+	def initialize_ui(self) -> None:
+		"""Initialize the user interface."""
+		self.setWindowTitle('3D matplotlib model test')
+		self.ui.framePlot.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+		self.chart = Canvas(self.ui.framePlot)
+		self.ui.chkAxes.clicked.connect(self.change_axes_state)
+
+	def draw_basic_model(self) -> None:
+		"""Draw a model of a capacitor."""
+		self.chart.draw_capacitor(2, [0, np.linspace(0, 1, 2), 0], [1, np.linspace(0, 1, 2), 0], 0.2)
+
+	def change_axes_state(self) -> None:
+		"""Switch XYZ axes."""
+		self.chart.clear_plot()
+
+		if self.ui.chkAxes.checkState() == Qt.Checked:
+			self.chart.draw_arrow_axes(0.1, True)
+			self.draw_basic_model()
+			return
+
+		self.chart.draw_arrow_axes(0.1, False)
+		self.draw_basic_model()
+
